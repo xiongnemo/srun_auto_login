3c3e25a726cb34c0d8c5301979ef868942562bb7
NemoMarshmallow <xiongnemo@126.com>
2021-01-04 15:38:38 +0800

    fix cron

--- cron.sh
+++ cron.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+# where are you?
+CURRENT_DIRECTORY = $PWD
 # write out current crontab to temp file
 crontab -l > temp_cron_file
 # echo new cron into cron file
-echo "0 */4 * * * python3 ./main.py " >> temp_cron_file
+echo "0 */4 * * * python3 $CURRENT_DIRECTORY/main.py" >> temp_cron_file
 # install new cron file
 crontab temp_cron_file
 # remove temp file
